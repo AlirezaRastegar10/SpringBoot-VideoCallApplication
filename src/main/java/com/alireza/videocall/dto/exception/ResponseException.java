8d465a3743038dda64c915bc7c00eebe28b44a2a
phylix <alirezarastegar117390@gmail.com>
2023-09-20 18:57:44 +0330

    create ResponseException dto for sending exception information

---
+++ src/main/java/com/alireza/videocall/dto/exception/ResponseException.java
@@ -0,0 +1,20 @@
+package com.alireza.videocall.dto.exception;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ResponseException {
+
+    Timestamp timestamp;
+    Integer status;
+    String error;
+    String message;
+    String path;
+}
